18882a277e5b416570e4d0efe74fe65e45c69f51
Ray Yuan <ray.x.yuan@gmail.com>
2018-01-02 22:54:30 -0800

    apply on training pipelines

--- similarity.py
+++ similarity.py
@@ -251,3 +251,78 @@ def get_cosine_similarity(q1_csc, q2_csc):
     return cosine_sim
 
 cosine_sim = get_cosine_similarity(q1_csc, q2_csc)
+
+from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn.svm import SVC
+from sklearn.ensemble import GradientBoostingClassifier as GBC
+from sklearn.linear_model import LogisticRegression as LR
+from sklearn.cross_validation import train_test_split
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import f1_score, confusion_matrix
+from sklearn.pipeline import Pipeline
+
+np.random.seed(10)
+
+def train_rfc(X,y):
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+
+    svm_models = [('svm', SVC(verbose=1, shrinking=False))]
+    svm_pipeline = Pipeline(svm_models)
+    svm_params = {'svm__kernel' : ['rbf'],
+                  'svm__C' : [0.01,0.1,1],
+                  'svm__gamma' :[0.1,0.2,0.4],
+                  'svm__tol' :[0.001,0.01,0.1],
+                  'svm__class_weight' : [{1:0.8,0:0.2}]}
+
+    rfc_models = [('rfc', RFC())]
+    rfc_pipeline = Pipeline(rfc_models)
+    rfc_params = {'rfc__n_estimators' : [40],
+                  'rfc__max_depth' : [40],
+                  'rfc__min_samples_leaf' : [50]}
+
+    lr_models = [('lr', LR(verbose=1))]
+    lr_pipeline = Pipeline(lr_models)
+    lr_params = {'lr__C': [0.1, 0.01],
+                 'lr__tol': [0.001,0.01],
+                 'lr__max_iter': [200,400],
+                 'lr__class_weight' : [{1:0.8,0:0.2}]}
+
+    gbc_models = [('gbc', GBC(verbose=1))]
+    gbc_pipeline = Pipeline(gbc_models)
+    gbc_params = {'gbc__n_estimators' : [100,200, 400, 800],
+                  'gbc__max_depth' : [40, 80, 160, 320],
+                  'gbc__learning_rate' : [0.01,0.1]}
+
+    grid = zip([svm_pipeline, rfc_pipeline, lr_pipeline, gbc_pipeline],
+               [svm_params, rfc_params, lr_params, gbc_params])
+
+    grid = zip([rfc_pipeline],
+               [rfc_params])
+
+    best_clf = None
+
+    for model_pipeline, param in grid:
+        temp = GridSearchCV(model_pipeline, param_grid=param, cv=4, scoring='f1')
+        temp.fit(X_train, y_train)
+
+        if best_clf is None:
+            best_clf = temp
+        else:
+            if temp.best_score_ > best_clf.best_score_:
+                best_clf = temp
+
+    model_details = {}
+    model_details["CV Accuracy"] = best_clf.best_score_
+    model_details["Model Parameters"] = best_clf.best_params_
+    model_details["Test Data Score"] = best_clf.score(X_test, y_test)
+    model_details["F1 score"] = f1_score(y_test, best_clf.predict(X_test))
+    model_details["Confusion Matrix"] = str(confusion_matrix(y_test, best_clf.predict(X_test)))
+
+    return best_clf, model_details
+X = np.array(cosine_sim).reshape(-1,1)
+y = df_train.is_duplicate
+
+clf, model_details = train_rfc(X,y)
+
+print (model_details)
